1199a2e14a520c2fc9c30f1ba4f6dd336f08131b
Aliaksei Siankevich <andfly@tut.by>
2020-10-06 15:10:47 +0300

    feat: add body function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  let matrixArray =[];
+  let reverseArray = [];
+ 
+ if(arguments.length){
+   for(let i = 0; i < matrix.length; i++){
+     if(i % 2 === 0) {
+       reverseArray.push(matrix[i]);
+     }else{
+       reverseArray.push(matrix[i].reverse());
+     }
+   }
+   matrixArray = reverseArray.flat(reverseArray.length);
+   return matrixArray;
+ }else{
+   return matrixArray;
+ }
 }
